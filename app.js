c5d191f19eae10ec1cbd46d7d806b05134e3898e
montikayoti <109443641+shinnjiwoong@users.noreply.github.com>
2022-12-26 23:45:45 +0900

    221226
    1. 메인화면 디자인 수정
    2. 필터링 기능 완성

--- app.js
+++ app.js
@@ -11,41 +11,52 @@ const all_project_btn = document.querySelector('.all-project-btn');
 const project_lists = document.querySelector('.index-projects-box');
 const cursor = document.querySelector('.cursor-container');
 const cursor_design = document.querySelector('.cursor');
+const intro_title_container = document.querySelector('.intro-title-container');
+let intro_title = document.querySelector('.intro-title-1')
+let title_slogans = ["신지웅의", "작업물을", "모아놓은", "공간입니다", "안녕하세요", ":)", "언제나", "환영합니다", "둘러보세요", "누구신지는", "모르겠지만", "반가워요", "오래", "머물다", "가세요", "저는", "음악과", "웹을", "사랑하는", "사람입니다"]
 let view_mode_flag = "home";
 let project_num = project_lists.children.length;
 // CURSOR ANIMATION
+let window_width = window.innerWidth;
+setInterval(function(){
+    intro_title.innerText = title_slogans[Math.floor(Math.random() * title_slogans.length)];
+    setTimeout(function(){
+        intro_title.innerText = "";
+    }, 500)
+}, 2000)
 window.addEventListener("mousemove", (e)=>{
+    intro_title_container.style.opacity = `${100/window_width*e.clientX}%`
     cursor.style.top = `${e.clientY}px`;
     cursor.style.left = `${e.clientX}px`;
     if(view_mode_flag == "home"){
-        cursor_design.style.border = "solid 1.5px rgb(0, 255, 0)"
-        cursor_design.style.backgroundColor = "rgb(0, 255, 0, 0.7)"
+        cursor_design.style.border = "solid 1.5px rgb(0, 0, 255)"
+        cursor_design.style.backgroundColor = "rgb(0, 0, 255, 0.2)"
         if(e.target.classList.contains("project-view-index")||e.target.classList.contains("project-view-home")||e.target.classList.contains("project-view-detail")||e.target.classList.contains("link")){
             cursor_design.style.width = "100px";
             cursor_design.style.height = "100px";
         }else{
             cursor_design.style.width = "20px";
             cursor_design.style.height = "20px";
-            cursor_design.style.backgroundColor = "rgb(0, 255, 0)"
+            cursor_design.style.backgroundColor = "rgb(0, 0, 255)"
         }
     }else if(view_mode_flag == "index"){
-        cursor_design.style.border = "solid 1.5px rgb(0, 255, 0)"
-        cursor_design.style.backgroundColor = "rgb(0, 255, 0, 0.7)"
-        if(e.target.classList.contains("project-view-index")||e.target.classList.contains("project-view-home")||e.target.classList.contains("project-view-detail")||e.target.classList.contains("link")){
+        cursor_design.style.border = "solid 1.5px rgb(0, 0, 255)"
+        cursor_design.style.backgroundColor = "rgb(0, 0, 255, 0.2)"
+        if(e.target.classList.contains("project-view-index")||e.target.classList.contains("project-view-home")||e.target.classList.contains("project-view-detail")||e.target.classList.contains("link")||e.target.classList.contains("all-project-btn")){
             cursor_design.style.width = "100px";
             cursor_design.style.height = "100px";
             }else if(e.target.classList.contains("index-tag-container")){
                 cursor_design.style.width = "100px";
                 cursor_design.style.height = "100px";
-                cursor_design.style.backgroundColor = "rgb(0, 255, 0, 0.7)"
+                cursor_design.style.backgroundColor = "rgb(0, 0, 255, 0.2)"
             }else{
                 cursor_design.style.width = "20px";
                 cursor_design.style.height = "20px";
-                cursor_design.style.backgroundColor = "rgb(0, 255, 0)"
+                cursor_design.style.backgroundColor = "rgb(0, 0, 255)"
             }
     }else if(view_mode_flag == "detail"){
-        cursor_design.style.border = "solid 1.5px rgb(0, 255, 0)"
-        cursor_design.style.backgroundColor = "rgb(0, 255, 0, 0.7)"
+        cursor_design.style.border = "solid 1.5px rgb(0, 0, 255)"
+        cursor_design.style.backgroundColor = "rgb(0, 0, 255, 0.2)"
         if(e.target.classList.contains("project-view-index")||e.target.classList.contains("project-view-home")||e.target.classList.contains("project-view-detail")||e.target.classList.contains("link")){
             cursor_design.style.width = "100px";
             cursor_design.style.height = "100px";
@@ -53,7 +64,7 @@ window.addEventListener("mousemove", (e)=>{
         else{
             cursor_design.style.width = "20px";
             cursor_design.style.height = "20px";
-            cursor_design.style.backgroundColor = "rgb(0, 255, 0)"
+            cursor_design.style.backgroundColor = "rgb(0, 0, 255)"
         }
     }
 })
@@ -74,12 +85,12 @@ index_btn.addEventListener('click', ()=>{
     intro_page.style.top = "-100vh";
     index_page.style.top = "0vh";
     detail_page.style.top = "100vh"
-    index_btn.style.color = "rgb(0, 255, 0)"
-    index_btn.style.border = "solid 1px rgb(0, 255, 0)"
-    home_btn.style.color = "rgb(0, 255, 0)"
-    home_btn.style.border = "solid 1px rgb(0, 255, 0)"
-    detail_btn.style.color = "rgb(0, 255, 0)"
-    detail_btn.style.border = "solid 1px rgb(0, 255, 0)"
+    index_btn.style.color = "rgb(0, 0, 255)"
+    index_btn.style.border = "solid 1px rgb(0, 0, 255)"
+    home_btn.style.color = "rgb(0, 0, 255)"
+    home_btn.style.border = "solid 1px rgb(0, 0, 255)"
+    detail_btn.style.color = "rgb(0, 0, 255)"
+    detail_btn.style.border = "solid 1px rgb(0, 0, 255)"
 })
 home_btn.addEventListener('click', ()=>{
     view_mode_flag = "home";
@@ -98,12 +109,12 @@ detail_btn.addEventListener("click", ()=>{
     intro_page.style.top = "-200vh";
     index_page.style.top = "-100vh";
     detail_page.style.top = "0vh";
-    index_btn.style.color = "rgb(0, 255, 0)"
-    index_btn.style.border = "solid 1px rgb(0, 255, 0)"
-    home_btn.style.color = "rgb(0, 255, 0)"
-    home_btn.style.border = "solid 1px rgb(0, 255, 0)"
-    detail_btn.style.color = "rgb(0, 255, 0)"
-    detail_btn.style.border = "solid 1px rgb(0, 255, 0)"
+    index_btn.style.color = "rgb(0, 0, 255)"
+    index_btn.style.border = "solid 1px rgb(0, 0, 255)"
+    home_btn.style.color = "rgb(0, 0, 255)"
+    home_btn.style.border = "solid 1px rgb(0, 0, 255)"
+    detail_btn.style.color = "rgb(0, 0, 255)"
+    detail_btn.style.border = "solid 1px rgb(0, 0, 255)"
 })
 
 // TAG FILTERING
@@ -129,10 +140,11 @@ function filtering_tags(tag){
 }
 window.addEventListener("click", (e)=>{
     // console.log(e.target)
-    all_project_btn.style.opacity = "100%";
     const classes = e.target.classList
     let tag_type = ""
     if(classes.contains("index-tag-container")){
+        all_project_btn.style.opacity = "100%";
+        all_project_btn.style.pointerEvents = "auto";
         tag_type = classes[1];
         filtering_tags(tag_type);
     }
@@ -144,4 +156,5 @@ all_project_btn.addEventListener("click", ()=>{
         project_lists.children[i].style.display = "block";
     }
     all_project_btn.style.opacity = "0%";
+    all_project_btn.style.pointerEvents = "none";
 })
