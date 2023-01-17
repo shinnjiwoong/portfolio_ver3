2afc4f39cfe4ddb611f370ce2268f8d5b504b948
montikayoti <109443641+shinnjiwoong@users.noreply.github.com>
2023-01-17 18:38:05 +0900

    230117
    1. 인트로 페이지 구현 완료
    2. 음악 플레이어 마크업 및 디자인 구현 완료

--- app.js
+++ app.js
@@ -1,34 +1,64 @@
 
 
 // VARIABLES
-const index_btn = document.querySelector('.project-view-index');
-const home_btn = document.querySelector('.project-view-home');
-const detail_btn = document.querySelector('.project-view-detail');
 const intro_page = document.querySelector('.intro-page-container');
-const index_page = document.querySelector('.index-page-container');
-const detail_page = document.querySelector('.detail-page-container');
-const all_project_btn = document.querySelector('.all-project-btn');
-const project_lists = document.querySelector('.index-projects-box');
-const top_container = document.querySelector(".top-container");
-const bottom_container = document.querySelector(".bottom-container");
 const cursor = document.querySelector('.cursor-container');
 const cursor_design = document.querySelector('.cursor-img');
+const bg_container = document.querySelector(".bg-container");
+const project_wrap = document.querySelector(".project-wrap");
+
+let view_mode_flag = "home";
+
+// MUSIC PLAYER VAR
+const music_bar_container = document.querySelector(".music-bar-container");
+const music_bar_track_title = document.querySelector(".music-bar-track-title");
+const track_titles = document.querySelectorAll(".track-title");
+const music_bar_playpause_btn = document.querySelector(".music-bar-playpause-btn");
+let playpause_flag = "pause";
+
+// --------------------------------------------------------------------------------------------
+
+// INTRO PAGE VAR
+const top_container = document.querySelector(".top-container");
+const bottom_container = document.querySelector(".bottom-container");
 const intro_title_container = document.querySelector('.intro-top-bg');
 const intro_top_right_bg = document.querySelector('.intro-top-right-bg');
 const intro_bottom = document.querySelector(".intro-bottom-bg");
 const intro_bottom_right_bg = document.querySelector(".intro-bottom-right-bg");
-const bg_container = document.querySelector(".bg-container");
-const change_btn = document.querySelector(".change-btn");
-const project_wrap = document.querySelector(".project-wrap");
-let title_slogans = ["portfolio", "website", ":)"]
-let view_mode_flag = "home";
-let project_num = project_lists.children.length;
 
-// INTRO PAGE VAR
-const intro_music_btn = document.querySelector(".intro-project-links-btn");
 const music_container = document.querySelector('.music-container');
 const bio_container = document.querySelector(".bio-container");
+// BTNS
 const intro_bio_btn = document.querySelector(".intro-project-bio-btn");
+const intro_work_btn = document.querySelector(".intro-project-overview-btn");
+const intro_music_btn = document.querySelector(".intro-project-links-btn");
+const intro_timeline_btn = document.querySelector(".intro-project-timeline-btn");
+
+// --------------------------------------------------------------------------------------------
+
+// <FUNCTIONS>
+
+
+// MUSIC BAR ANIMATIONS
+track_titles.forEach(t => t.addEventListener('click', (e)=>{
+    playpause_flag = "play";
+    music_bar_track_title.innerHTML = `${e.target.innerHTML}`
+    showMusicBar();
+    setTimeout(hideMusicBar, 2000);
+}))
+music_bar_playpause_btn.addEventListener("click", ()=>{
+    if(playpause_flag == "play"){
+        playpause_flag = "pause"
+    }else{
+        playpause_flag = "play"
+    }
+})
+function showMusicBar(){
+    music_bar_container.style.bottom = '0';
+}
+function hideMusicBar(){
+    music_bar_container.style.bottom = "-10%";
+}
 
 // INTRO BIO SHOWUP
 intro_bio_btn.addEventListener("click", ()=>{
@@ -40,68 +70,94 @@ intro_music_btn.addEventListener('click', ()=>{
     intro_music_btn.style.display = 'none';
     music_container.style.display = 'block'
 })
-// CHANGE THE BG
-change_btn.addEventListener("click", ()=>{
-    bg_container.style.zIndex = "100";
-    intro_title_container.style.width = "50vw";
-    intro_title_container.style.height = "100vh";
-    intro_title_container.style.position = "absolute";
-    intro_bottom.style.width = "50vw";
-    intro_bottom.style.height = "100vh";
-    intro_bottom.style.position = "absolute";
-    intro_bottom.style.left = "50vw";
-    intro_bottom.style.top = "0vh";
-    top_container.style.display = 'none';
-    bottom_container.style.display = 'none';
+
+// WORK & TIMELINE BTN CLICK TRANSITION
+function hideIntroContents(){
+    window.removeEventListener('click', handleWindowClick);
+    intro_title_container.style.backgroundColor = "rgb(255, 255, 255)"
+    intro_top_right_bg.style.backgroundColor = "rgb(200, 200, 200)";
+    intro_bottom.style.backgroundColor = "rgb(200, 200, 200)";
+    intro_bottom_right_bg.style.backgroundColor = "rgb(255, 255, 255)";
+    top_container.style.opacity = "0%"
+    bottom_container.style.opacity = "0%";
+
     setTimeout(function(){
-        project_wrap.style.opacity = "100%"
-        project_wrap.style.zIndex = "110";
-    }, 500);
-})
+        top_container.style.display = "none";
+        bottom_container.style.display = "none";
+    }, 1000)
+}
+
+intro_work_btn.addEventListener('click', hideIntroContents);
+intro_timeline_btn.addEventListener('click', hideIntroContents)
 
 // CURSOR ANIMATION
 let window_width = window.innerWidth;
 let window_height = window.innerHeight;
-// setInterval(function(){
-//     intro_title_container.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-//     // intro_title.innerText = title_slogans[Math.floor(Math.random() * title_slogans.length)];
-//     let r = Math.floor(Math.random() * 255);
-//     let g = Math.floor(Math.random() * 255);
-//     let b = Math.floor(Math.random() * 255);
-//     intro_bottom.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-// }, 2000)
 
-window.addEventListener('click', ()=>{
+window.addEventListener('click', handleWindowClick);
+function handleWindowClick(){
     randomColorChange(intro_title_container)
     randomColorChange(intro_top_right_bg)
     randomColorChange(intro_bottom)
     randomColorChange(intro_bottom_right_bg)
-});
+}
 function randomColorChange(e){
     console.log('clicked!')
     e.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 }
 window.addEventListener("mousemove", (e)=>{
-    // intro top & bottom color change
-    // intro_title_container.style.opacity = `${100/window_width*e.clientX}%`;
-    // intro_bottom.style.opacity = `${100/window_height*e.clientY}%`
     // Cursor change
     cursor.style.top = `${e.clientY}px`;
     cursor.style.left = `${e.clientX}px`;
+    console.log(window.innerHeight, e.clientY)
+
+    // 커서의 위치에 기반하여 음악 플레이어를 보여주는 함수
+    if(window.innerHeight - e.clientY < 30){
+        showMusicBar();
+    }else{
+        hideMusicBar();
+    }
+
+    // 요소에 따른 커서의 모양변화
     if(view_mode_flag == "home"){
         // cursor_design.style.border = "solid 1.5px rgb(25, 25, 25)"
         // cursor_design.style.backgroundColor = "rgb(25, 25, 25, 0.2)"
-        if(e.target.classList.contains("intro-project-index-btn")||e.target.classList.contains("intro-project-links-btn")||e.target.classList.contains("intro-project-overview-btn")||e.target.classList.contains("intro-project-bio-btn")||e.target.classList.contains("track-title")){
+        if(e.target.classList.contains("intro-project-timeline-btn")||e.target.classList.contains("intro-project-links-btn")||e.target.classList.contains("intro-project-overview-btn")||e.target.classList.contains("intro-project-bio-btn")||e.target.classList.contains("track-title")){
             cursor_design.setAttribute('src', 'assets/web_logo/cursor/main_cursor.png')
-            cursor_design.style.animation = "rotate_image 2s linear infinite"
-            cursor_design.style.width = "30px"
-            cursor_design.style.height = "30px"
+            cursor_design.style.animation = "rotate_image 1s linear infinite"
+            cursor_design.style.width = "60px"
+            cursor_design.style.height = "60px"
         }
         else if(e.target.classList.contains("link")){
             cursor_design.setAttribute('src', 'assets/web_logo/cursor/link.png')
             cursor_design.style.animation = "none"
-            cursor_design.style.width = "30px"
-            cursor_design.style.height = "30px"
+            cursor_design.style.width = "40px"
+            cursor_design.style.height = "40px"
+        }
+        else if(e.target.classList.contains("music-bar-playpause-btn")){
+            if(playpause_flag == "play"){
+                cursor_design.setAttribute('src', 'assets/web_logo/cursor/pause.png')
+                cursor_design.style.animation = "none"
+                cursor_design.style.width = "60px"
+                cursor_design.style.height = "60px"
+            }else if(playpause_flag == "pause"){
+                cursor_design.setAttribute('src', 'assets/web_logo/cursor/play.png')
+                cursor_design.style.animation = "none"
+                cursor_design.style.width = "60px"
+                cursor_design.style.height = "60px"
+            }
+        }
+        else if(e.target.classList.contains("music-bar-prev-btn")){
+            cursor_design.setAttribute('src', 'assets/web_logo/cursor/arrow_l.png')
+            cursor_design.style.animation = "none"
+            cursor_design.style.width = "70px"
+            cursor_design.style.height = "70px"
+        }
+        else if(e.target.classList.contains("music-bar-next-btn")){
+            cursor_design.setAttribute('src', 'assets/web_logo/cursor/arrow_r.png')
+            cursor_design.style.animation = "none"
+            cursor_design.style.width = "70px"
+            cursor_design.style.height = "70px"
         }
         else{
             cursor_design.setAttribute('src', 'assets/web_logo/cursor/esc.png')
@@ -109,128 +165,5 @@ window.addEventListener("mousemove", (e)=>{
             cursor_design.style.width = "20px"
             cursor_design.style.height = "20px"
         }
-    }else if(view_mode_flag == "index"){
-        cursor_design.style.border = "solid 1.5px rgb(25, 25, 25)"
-        // cursor_design.style.backgroundColor = "rgb(25, 25, 25, 0.2)"
-        if(e.target.classList.contains("project-view-index")||e.target.classList.contains("project-view-home")||e.target.classList.contains("project-view-detail")||e.target.classList.contains("link")||e.target.classList.contains("all-project-btn")){
-            cursor_design.style.width = "100px";
-            cursor_design.style.height = "100px";
-            cursor_design.style.borderRadius = "100%"
-            }else if(e.target.classList.contains("index-tag-container")){
-                cursor_design.style.width = "100px";
-                cursor_design.style.height = "100px";
-                cursor_design.style.borderRadius = "100%"
-                // cursor_design.style.backgroundColor = "rgb(25, 25, 25, 0.2)"
-            }else{
-                cursor_design.style.width = "10px";
-                cursor_design.style.height = "10px";
-                cursor_design.style.borderRadius = "0%"
-                // cursor_design.style.backgroundColor = "rgb(25, 25, 25)"
-            }
-    }else if(view_mode_flag == "detail"){
-        cursor_design.style.border = "solid 1.5px rgb(250, 250, 250)"
-        // cursor_design.style.backgroundColor = "rgb(250, 250, 250, 0.2)"
-        if(e.target.classList.contains("project-view-index")||e.target.classList.contains("project-view-home")||e.target.classList.contains("project-view-detail")||e.target.classList.contains("detail-left-click")||e.target.classList.contains("detail-right-click")){
-            cursor_design.style.width = "100px";
-            cursor_design.style.height = "100px";
-            cursor_design.style.borderRadius = "100%"
-            }
-        else{
-            cursor_design.style.width = "10px";
-            cursor_design.style.height = "10px";
-            cursor_design.style.borderRadius = "0%"
-            cursor_design.style.backgroundColor = "rgb(250, 250, 250)"
-        }
     }
-})
-
-// INTRO PAGE ANIMATION
-window.addEventListener("load", ()=>{
-    intro_page.style.top = "0vh";
-    index_page.style.top = "100vh";
-    detail_page.style.top = "200vh"
-    index_btn.style.color = "rgb(25, 25, 25)"
-    index_btn.style.border = "solid 1px rgb(25, 25, 25)"
-    home_btn.style.color = "rgb(25, 25, 25)"
-    home_btn.style.border = "solid 1px rgb(25, 25, 25)"
-    detail_btn.style.color = "rgb(25, 25, 25)"
-    detail_btn.style.border = "solid 1px rgb(25, 25, 25)"
-})
-index_btn.addEventListener('click', ()=>{
-    view_mode_flag = "index";
-    intro_page.style.top = "-100vh";
-    index_page.style.top = "0vh";
-    detail_page.style.top = "100vh"
-    index_btn.style.color = "rgb(25, 25, 25)"
-    index_btn.style.border = "solid 1px rgb(25, 25, 25)"
-    home_btn.style.color = "rgb(25, 25, 25)"
-    home_btn.style.border = "solid 1px rgb(25, 25, 25)"
-    detail_btn.style.color = "rgb(25, 25, 25)"
-    detail_btn.style.border = "solid 1px rgb(25, 25, 25)"
-})
-home_btn.addEventListener('click', ()=>{
-    view_mode_flag = "home";
-    intro_page.style.top = "0vh";
-    index_page.style.top = "100vh";
-    detail_page.style.top = "200vh"
-    index_btn.style.color = "rgb(25, 25, 25)"
-    index_btn.style.border = "solid 1px rgb(25, 25, 25)"
-    home_btn.style.color = "rgb(25, 25, 25)"
-    home_btn.style.border = "solid 1px rgb(25, 25, 25)"
-    detail_btn.style.color = "rgb(25, 25, 25)"
-    detail_btn.style.border = "solid 1px rgb(25, 25, 25)"
-})
-detail_btn.addEventListener("click", ()=>{
-    view_mode_flag = "detail";
-    intro_page.style.top = "-200vh";
-    index_page.style.top = "-100vh";
-    detail_page.style.top = "0vh";
-    index_btn.style.color = "rgb(250, 250, 250)"
-    index_btn.style.border = "solid 1px rgb(250, 250, 250)"
-    home_btn.style.color = "rgb(250, 250, 250)"
-    home_btn.style.border = "solid 1px rgb(250, 250, 250)"
-    detail_btn.style.color = "rgb(250, 250, 250)"
-    detail_btn.style.border = "solid 1px rgb(250, 250, 250)"
-})
-
-// TAG FILTERING
-function filtering_tags(tag){
-    console.log(tag)
-    
-    for(let i = 0; i<project_num; i++){
-        let flag = false
-        let tag_childs = project_lists.children[i].children[0].children[3].children;
-        let child_num = project_lists.children[i].children[0].children[3].children.length;
-        for(let j = 0; j<child_num; j++){
-            // console.log(tag_childs[j].classList[1])
-            if(tag_childs[j].classList[1] == tag){
-                flag = true
-                project_lists.children[i].style.display = "block";
-                break;
-            };
-        }
-        if(flag == false){
-            project_lists.children[i].style.display = "none";
-        }
-    }
-}
-window.addEventListener("click", (e)=>{
-    // console.log(e.target)
-    const classes = e.target.classList
-    let tag_type = ""
-    if(classes.contains("index-tag-container")){
-        all_project_btn.style.opacity = "100%";
-        all_project_btn.style.pointerEvents = "auto";
-        tag_type = classes[1];
-        filtering_tags(tag_type);
-    }
-    
-})
-
-all_project_btn.addEventListener("click", ()=>{
-    for(let i = 0; i<project_num; i++){
-        project_lists.children[i].style.display = "block";
-    }
-    all_project_btn.style.opacity = "0%";
-    all_project_btn.style.pointerEvents = "none";
 })
